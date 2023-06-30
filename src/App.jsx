dc6a22710b43fb38066cf5514306912f0bbb7813
lehung2022 <hung.le.programmer@gmail.com>
2023-06-30 13:36:34 +0700

    new bugs and new ways to fix the bugs with Routes, navigate in React Router Version 6

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import Home from "./components/home/Home";
+import NewPost from "./components/posts/NewPost";
+import PostPage from "./components/posts/PostPage";
+import About from "./components/about/About";
+import EditPost from "./components/edit/EditPost";
+import Missing from "./components/missing/Missing";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+import './index.css';
+import api from "./components/api/api";
+
+function App() {
+  const [posts, setPosts] = useState([])
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    }
+
+    fetchPosts();
+  }, [])
+
+  useEffect(() => {
+    const filteredResults = posts?.filter((post) =>
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts?.length ? posts[posts?.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts?.map(post => post?.id === id ? { ...response.data } : post));
+      setEditTitle('');
+      setEditBody('');
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts?.filter(post => post.id !== id);
+      setPosts(postsList);
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+
+  return (
+    <>
+      <div className="App">
+        <Header title="React JS Blog" />
+        <Nav search={search} setSearch={setSearch} />
+        <Routes>
+          <Route exact path="/" element={<Home posts={searchResults} />}>
+          </Route>
+          <Route exact path="/post" element={<NewPost
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+          />}>
+
+          </Route>
+          <Route path="/edit/:id" element={<EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
+          />}>
+
+          </Route>
+          <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete} />}>
+
+          </Route>
+          <Route path="/about" component={About} />
+          <Route path="*" component={Missing} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App;
